ff68ed6169fb54503a6a0066b549c5f36936e6fe
Dharmit Shah <dharmitshah009@gmail.com>
2022-03-20 20:51:53 -0700

    Update GUIcirclePropertyCalculator.py

--- Basics/tkinter/GUIcirclePropertyCalculator.py
+++ Basics/tkinter/GUIcirclePropertyCalculator.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+# calculates area and circumference of the circle.
 def calc():
     r=int(radF.get())
     print(r);
@@ -8,23 +9,27 @@ def calc():
     areF.insert(0, a);
     cirF.insert(0, c);
 
+# Adding clear function ...
 def clear():
     radF.delete(0,100);
     areF.delete(0,100);
     cirF.delete(0,100);
     
     
-
+## Creating mainframe ...
 master = Tk();
 
+# creating 3 labels
 radL = Label(master, text="Radius: ");
 areL = Label(master, text="Areaa: ");
 cirL = Label(master, text="Circumference: ");
 
+# creating 3 text fields
 radF = Entry(master)
 areF = Entry(master)
 cirF = Entry(master)
 
+# Creating 2 buttons
 clearBtn = Button(master, text="Clear", command=clear);
 calBtn   = Button(master, text="Calculate", command=calc);
 
